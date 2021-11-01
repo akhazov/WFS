b68c296a9fe67e1ed700402c18305c7715832485
akhazow <aleks.khazow@gmail.com>
2021-11-01 14:19:07 +0300

    WFS-8 Рефакторинг ответа сервиса.

--- src/main/java/com/drmun/weather/weatherservice/controller/WeatherEndpoint.java
+++ src/main/java/com/drmun/weather/weatherservice/controller/WeatherEndpoint.java
@@ -1,9 +1,9 @@
 package com.drmun.weather.weatherservice.controller;
 
-import com.drmun.weather.weatherservice.controller.dto.Response;
-import com.drmun.weather.weatherservice.model.dto.WeatherDTO;
 import com.drmun.weather.weatherservice.service.WeatherService;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,9 +19,7 @@ public class WeatherEndpoint {
     }
 
     @GetMapping(path = "temp", produces = MediaType.APPLICATION_JSON_VALUE)
-    public Response getTempByCity(String city) {
-        return Response.builder()
-                .content(service.getCurrentTempByCity(city))
-                .build();
+    public ResponseEntity<?> getTempByCity(String city) {
+        return ResponseEntity.ok(service.getCurrentTempByCity(city));
     }
 }
--- src/test/java/com/drmun/weather/weatherservice/controller/WeatherEndpointTest.java
+++ src/test/java/com/drmun/weather/weatherservice/controller/WeatherEndpointTest.java
@@ -1,23 +1,13 @@
 package com.drmun.weather.weatherservice.controller;
 
-import com.drmun.weather.weatherservice.controller.dto.Response;
-import com.drmun.weather.weatherservice.model.dto.CityTemp;
-import com.drmun.weather.weatherservice.model.openweater.Main;
-import com.drmun.weather.weatherservice.model.openweater.OpenWeatherMapResponse;
-import com.drmun.weather.weatherservice.repository.CurrentWeatherRepository;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.test.web.reactive.server.WebTestClient;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @ExtendWith(MockitoExtension.class)
